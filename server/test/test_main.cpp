fbeb4fd64ce4a9094f0a0d4055c2a97bed9c2aa5
szokolai-mate <sirmate@gmail.com>
2017-04-20 14:24:58 +0200

    Seperated build targets, coverage environment set up

--- server/test/test_main.cpp
+++ server/test/test_main.cpp
@@ -3,8 +3,16 @@
 
 int main(int argc, char **argv)
 {
+    if(argc>1){
+        if(std::string(argv[1])=="-s"){
+            std::cout<<"Running tests silently."<<std::endl;
+            ::testing::InitGoogleTest(&argc, argv);
+            RUN_ALL_TESTS();
+            return 0;
+        }
+    }
     ::testing::InitGoogleTest(&argc, argv);
-    auto res =  RUN_ALL_TESTS();
+    auto res = RUN_ALL_TESTS();
     std::cin.get();
     return res;
 }
